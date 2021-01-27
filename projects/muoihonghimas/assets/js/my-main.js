61b5601dbdc202f452248432964ffce2fb685ee4
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2021-01-28 00:02:54 +0700

    Update links file my-main.js

--- projects/muoihonghimas/assets/js/my-main.js
+++ projects/muoihonghimas/assets/js/my-main.js
@@ -1,15 +1,15 @@
 var test = ``;
 
-document.write('<script src="assets/js/header-mobi.js"></script>');
-document.write('<script src="assets/js/vs-menu-wrapper.js"></script>');
-document.write('<script src="assets/js/header-desk.js"></script>');
-document.write('<script src="assets/js/footer.js"></script>');
-document.write('<script src="assets/js/vendor/jquery-1.12.4.min.js"></script>');
-document.write('<script src="assets/js/app.min.js"></script>');
-document.write('<script src="assets/js/vscustom-carousel.min.js"></script>');
-document.write('<script src="assets/js/vsmenu.min.js"></script>');
-document.write('<script src="assets/js/ajax-mail.js"></script>');
+document.write('<script src="https://namdp1999.github.io/projects/muoihonghimas/assets/js/header-mobi.js"></script>');
+document.write('<script src="https://namdp1999.github.io/projects/muoihonghimas/assets/js/vs-menu-wrapper.js"></script>');
+document.write('<script src="https://namdp1999.github.io/projects/muoihonghimas/assets/js/header-desk.js"></script>');
+document.write('<script src="https://namdp1999.github.io/projects/muoihonghimas/assets/js/footer.js"></script>');
+document.write('<script src="https://namdp1999.github.io/projects/muoihonghimas/assets/js/vendor/jquery-1.12.4.min.js"></script>');
+document.write('<script src="https://namdp1999.github.io/projects/muoihonghimas/assets/js/app.min.js"></script>');
+document.write('<script src="https://namdp1999.github.io/projects/muoihonghimas/assets/js/vscustom-carousel.min.js"></script>');
+document.write('<script src="https://namdp1999.github.io/projects/muoihonghimas/assets/js/vsmenu.min.js"></script>');
+document.write('<script src="https://namdp1999.github.io/projects/muoihonghimas/assets/js/ajax-mail.js"></script>');
 document.write('<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDC3Ip9iVC0nIxC6V14CKLQ1HZNF_65qEQ"></script>');
-document.write('<script src="assets/js/map.js"></script>');
-document.write('<script src="assets/js/vs-colorplate.min.js"></script>');
-document.write('<script src="assets/js/main.js"></script>');
+document.write('<script src="https://namdp1999.github.io/projects/muoihonghimas/assets/js/map.js"></script>');
+document.write('<script src="https://namdp1999.github.io/projects/muoihonghimas/assets/js/vs-colorplate.min.js"></script>');
+document.write('<script src="https://namdp1999.github.io/projects/muoihonghimas/assets/js/main.js"></script>');
