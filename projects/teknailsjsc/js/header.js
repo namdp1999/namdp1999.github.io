650401d7654bf71a2b80f9e75e357e992277efb0
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2021-01-28 13:21:40 +0700

    Update links

--- projects/teknailsjsc/js/header.js
+++ projects/teknailsjsc/js/header.js
@@ -11,19 +11,19 @@ var header = `
       <div class="header-nav navbar-collapse collapse">
         <ul class="nav navbar-nav">
           <li><a href="index.html">Trang chủ</a></li>
-          <li><a href="#">Giới thiệu</a></li>
+          <li><a href="pages/gioi-thieu.htm">Giới thiệu</a></li>
           <li class="active"> <a href="javascript:;">Sản phẩm<i class="fa fa-chevron-down"></i></a>
             <ul class="sub-menu">
-              <li> <a href="#">Kềm Cắt Da</a></li>
-              <li> <a href="#">Kềm Cắt Móng</a></li>
-              <li> <a href="#">Sủi Da</a></li>
-              <li> <a href="#">Giũa</a></li>
-              <li> <a href="#">Móc Khóe</a></li>
+              <li> <a href="nhom-san-pham/kem-cat-da.htm">Kềm Cắt Da</a></li>
+              <li> <a href="nhom-san-pham/kem-cat-mong.htm">Kềm Cắt Móng</a></li>
+              <li> <a href="nhom-san-pham/sui-da.htm">Sủi Da</a></li>
+              <li> <a href="nhom-san-pham/giua.htm">Giũa</a></li>
+              <li> <a href="nhom-san-pham/moc-khoe.htm">Móc Khóe</a></li>
             </ul>
           </li>
-          <li><a href="#">Album ảnh</a></li>
-          <li><a href="#">Video</a></li>
-          <li><a href="#">Liên hệ</a></li>
+          <li><a href="pages/album-anh.htm">Album ảnh</a></li>
+          <li><a href="pages/video.htm">Video</a></li>
+          <li><a href="pages/lien-he.htm">Liên hệ</a></li>
         </ul>
       </div>
     </div>
