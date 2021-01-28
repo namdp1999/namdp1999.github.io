6b72bd44bc01f0745d7587e1aa220b886187174b
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2021-01-28 20:29:57 +0700

    Add FAQ

---
+++ projects/teknailsjsc/js/faq.js
@@ -0,0 +1,111 @@
+var faq = `
+<div class="container">
+  <div class="section-head text-center">
+    <h2 class="text-primary-2 spa-title-head ">Câu hỏi thường gặp</h2>
+    <div class="circle-sap bg-primary"></div>
+  </div>
+  <div class="row">
+    <div class="col-md-6">
+      <div class="dez-accordion box-sort-in m-b30 purpal" id="accordion1">
+        <div class="panel">
+          <div class="acod-head">
+            <h5 class="acod-title">
+              <a data-toggle="collapse" href="#collapseOne1" class="collapsed" data-parent="#accordion1">
+                <i class="fa fa-globe"></i> Quy trình đặt hàng như thế nào?
+              </a>
+            </h5>
+          </div>
+          <div id="collapseOne1" class="acod-body collapse">
+            <div class="acod-content">
+              <p><strong>Để đặt hàng Quý Khách làm theo 4&nbsp;bước sau đây:</strong></p>
+              <ul>
+                <li>Bước 1: Gọi điện hoặc điền Form đăng ký tư vấn</li>
+                <li>Bước 2: Ký hợp đồng (nếu khách hàng là doanh nghiệp) hoặc đặt hàng trên website nếu là đại lý hoặc khách mua lẻ</li>
+                <li>Bước 3: Thanh toán (đặt cọc từ 30% đến 50% nếu là doanh nghiệp) hoặc thanh toán 100% nếu là đại lý hoặc khách lẻ</li>
+                <li>Bước 4: Nhận hàng (Thanh toán % còn lại nếu là doanh nghiệp)</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="panel">
+          <div class="acod-head">
+            <h5 class="acod-title">
+              <a data-toggle="collapse" href="#collapseTwo1" class="collapsed" data-parent="#accordion1">
+                <i class="fa fa-globe"></i> Cách đặt hàng qua website như thế nào?
+              </a>
+            </h5>
+          </div>
+          <div id="collapseTwo1" class="acod-body collapse">
+            <div class="acod-content">
+              <p>Quý Khách xem từng nhóm quà phù hợp với người nhận quà và đi vào xem chi tiết từng sản phẩm, sau đó Quý Khách đặt số lượng và thanh toán. Chúng tôi đưa ra Form đặt hàng nhanh nên Quý Khách có thể cùng lúc đặt nhiều giỏ quả
+                hay hộp quà&nbsp;khác nhau trong một Form đăng ký, như vật sẽ rất tiết kiệm thời gian cho Quý Khách.<br>
+                Trong trường hợp Quý Khách không quen đặt hàng qua website thì có thể liên hệ qua điện thoại để chúng tôi ghi nhận đơn đặt hàng của Quý Khách, sau đó chúng tôi sẽ lập đơn hàng và gửi cho Quý Khách qua Email và qua Zalo. Quý
+                Khách xác nhận đơn hàng xong thì chúng tôi sẽ phục vụ Quý Khách.</p>
+            </div>
+          </div>
+        </div>
+        <div class="panel">
+          <div class="acod-head">
+            <h5 class="acod-title">
+              <a data-toggle="collapse" href="#collapseThree1" class="collapsed" data-parent="#accordion1">
+                <i class="fa fa-globe"></i> Quyền lợi khi làm đại lý phân phối là gì?
+              </a>
+            </h5>
+          </div>
+          <div id="collapseThree1" class="acod-body collapse">
+            <div class="acod-content">
+              <p><strong>Khi bạn là đại lý của Quà Tết 2021 bạn sẽ nhận được các quyền lợi sau:</strong></p>
+              <ul>
+                <li>Nhận ngay hệ thống marketing online thông minh để tiếp cận khách hàng (5 phút - Miễn phí)</li>
+                <li>Nhận ngay 01 khóa huấn luyện qua Zoom để bạn phát triển cho mình nhiều Cộng Tác Viên bán hàng cho bạn (30 phút - Miễn Phí)</li>
+                <li>Nhận ngay 03 công cụ marketing cao cấp để bạn tiếp cận hàng trăm khách hàng mỗi ngày với phương trâm không SPAM.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="panel">
+          <div class="acod-head">
+            <h5 class="acod-title">
+              <a data-toggle="collapse" href="#collapse4" class="collapsed" data-parent="#accordion1">
+                <i class="fa fa-globe"></i> Khi mua hàng có hóa đơn tài chính khi mua hàng không?
+              </a>
+            </h5>
+          </div>
+          <div id="collapse4" class="acod-body collapse">
+            <div class="acod-content">
+              <p>Chúng tôi cam kết cung cấp hóa đơn tài chính (Hóa đơn GTGT) cho bất kỳ đối tượng khách hàng nào. Hóa đơn được xuất ngay sau khi Quý Khách đã thanh toán 100% và được gửi qua Email.</p>
+            </div>
+          </div>
+        </div>
+        <div class="panel">
+          <div class="acod-head">
+            <h5 class="acod-title">
+              <a data-toggle="collapse" href="#collapse5" class="collapsed" data-parent="#accordion1">
+                <i class="fa fa-globe"></i> Đặt giỏ hàng theo ý riêng được không?
+              </a>
+            </h5>
+          </div>
+          <div id="collapse5" class="acod-body collapse">
+            <div class="acod-content">
+              <p><strong>Quý Khách muốn đặt hàng theo ý riêng hoàn toàn được:</strong></p>
+              <ul>
+                <li>Bước 1: Xem thư viện ảnh để biết các sản phẩm lẻ</li>
+                <li>Bước 2: Chọn sản phẩm và ghi lại mã số sản phẩm (Mã số ghi trên ảnh)</li>
+                <li>Bước 3: Liên hệ với chúng tôi qua Hotline 0917247247</li>
+                <li>Bước 4: Thống nhất đơn hàng và hoàn thành đơn hàng</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="faq_img">
+        <img src="images/banner/faq.png" alt="">
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
+document.getElementById("faq").innerHTML = faq;
