866476d6f85a50f95ea1a33b6ae7454d6a9c4ff8
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2021-01-28 14:14:03 +0700

    Update links

--- projects/teknailsjsc/js/header.js
+++ projects/teknailsjsc/js/header.js
@@ -11,19 +11,19 @@ var header = `
       <div class="header-nav navbar-collapse collapse">
         <ul class="nav navbar-nav">
           <li><a href="index.html">Trang chủ</a></li>
-          <li><a href="pages/gioi-thieu.htm">Giới thiệu</a></li>
+          <li><a href="https://teknailsjsc.com/pages/gioi-thieu.htm">Giới thiệu</a></li>
           <li class="active"> <a href="javascript:;">Sản phẩm<i class="fa fa-chevron-down"></i></a>
             <ul class="sub-menu">
-              <li> <a href="nhom-san-pham/kem-cat-da.htm">Kềm Cắt Da</a></li>
-              <li> <a href="nhom-san-pham/kem-cat-mong.htm">Kềm Cắt Móng</a></li>
-              <li> <a href="nhom-san-pham/sui-da.htm">Sủi Da</a></li>
-              <li> <a href="nhom-san-pham/giua.htm">Giũa</a></li>
-              <li> <a href="nhom-san-pham/moc-khoe.htm">Móc Khóe</a></li>
+              <li> <a href="https://teknailsjsc.com/nhom-san-pham/kem-cat-da.htm">Kềm Cắt Da</a></li>
+              <li> <a href="https://teknailsjsc.com/nhom-san-pham/kem-cat-mong.htm">Kềm Cắt Móng</a></li>
+              <li> <a href="https://teknailsjsc.com/nhom-san-pham/sui-da.htm">Sủi Da</a></li>
+              <li> <a href="https://teknailsjsc.com/nhom-san-pham/giua.htm">Giũa</a></li>
+              <li> <a href="https://teknailsjsc.com/nhom-san-pham/moc-khoe.htm">Móc Khóe</a></li>
             </ul>
           </li>
-          <li><a href="pages/album-anh.htm">Album ảnh</a></li>
-          <li><a href="pages/video.htm">Video</a></li>
-          <li><a href="pages/lien-he.htm">Liên hệ</a></li>
+          <li><a href="https://teknailsjsc.com/pages/album-anh.htm">Album ảnh</a></li>
+          <li><a href="https://teknailsjsc.com/pages/video.htm">Video</a></li>
+          <li><a href="https://teknailsjsc.com/pages/lien-he.htm">Liên hệ</a></li>
         </ul>
       </div>
     </div>
