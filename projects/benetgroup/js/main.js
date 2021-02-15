73b22fc50701cf5ef0739a0149a562c6bfe958ae
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2021-02-15 19:31:48 +0700

    Update links js

--- projects/benetgroup/js/main.js
+++ projects/benetgroup/js/main.js
@@ -1,6 +1,6 @@
 var test = ``;
 
-document.write('<script type="text/javascript" src="js/js-animate.js"></script>');
-document.write('<script type="text/javascript" src="js/js-jquery.js"></script>');
-document.write('<script src="js/js-app.js"></script>');
-document.write('<script type="text/javascript" src="js/js-validate.js"></script>');
+document.write('<script type="text/javascript" src="https://namdp1999.github.io/projects/benetgroup/js/js-animate.js"></script>');
+document.write('<script type="text/javascript" src="https://namdp1999.github.io/projects/benetgroup/js/js-jquery.js"></script>');
+document.write('<script src="https://namdp1999.github.io/projects/benetgroup/js/js-app.js"></script>');
+document.write('<script type="text/javascript" src="https://namdp1999.github.io/projects/benetgroup/js/js-validate.js"></script>');
