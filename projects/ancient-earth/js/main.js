ef2dec580064ff17cf302294180478e77ce1e736
Nam Dang <61955753+namdp1999@users.noreply.github.com>
2021-06-29 18:00:44 +0700

    Update code

--- projects/ancient-earth/js/main.js
+++ projects/ancient-earth/js/main.js
@@ -46,7 +46,7 @@
   if (window.location.hash) {
     startingYear = parseInt(window.location.hash.slice(1));
   }
-  yearsago.value = startingYear === 0 ? '0' : startingYear + ' triệu';
+  yearsago.value = startingYear === 0 ? '0' : startingYear;
   updateSelectWithValue(startingYear);
   onYearsAgoChanged();
 
@@ -143,7 +143,7 @@
 
     var jumpToElt = document.getElementById('jump-to');
     jumpToElt.onchange = function(e) {
-      yearsago.value = jumpToElt.value + ' triệu';
+      yearsago.value = jumpToElt.value;
       onYearsAgoChanged();
     };
   }
